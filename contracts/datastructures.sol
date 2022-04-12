0232d392f63d887a47ae17be0488e73916f082b4
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2022-04-12 19:21:08 +0300

    fix due to review

--- contracts/datastructures.sol
+++ contracts/datastructures.sol
@@ -15,24 +15,19 @@ interface IDataStructurePractice {
     function getMyInfo() external view returns(User memory);
 }
 
-contract DataStructuresPractice is IDataStructurePractice,Ownable {
-    mapping(address => User) users;
-    mapping(address => bool) exists;
 
-    function setNewUser(address _userAdr, User calldata _newUser) external override {
-        require(owner() == msg.sender);
-        require(!exists[_userAdr]);
+contract DataStructuresPractice is IDataStructurePractice, Ownable {
+    mapping(address => User) private users;
+
+    function setNewUser(address _userAdr, User calldata _newUser) external override onlyOwner {
         users[_userAdr] = _newUser;
-        exists[_userAdr] = true;
     }
 
     function getUser(address _user) external view override returns(User memory) {
-        require(exists[_user]);
         return users[_user];
     }
 
     function getMyInfo() external view override returns(User memory) {
-        require(exists[msg.sender]);
         return users[msg.sender];
     }
 }
